fc6bed9af2128f9cbb8a953fb29c5e843c718b7e
wzj <wzj@69521577-142b-c74d-ab66-1cd52e709b53>
2017-11-07 02:49:10 +0000

    git-svn-id: https://39.108.144.9:8443/svn/tianyou@161 69521577-142b-c74d-ab66-1cd52e709b53

--- includes/modules/payment/upop.php
+++ includes/modules/payment/upop.php
@@ -1,18 +1,17 @@
 <?php
 
 /**
- * ECSHOP 银联在线支付
- * ============================================================================
- * 版权所有 2005-2010 上海商派网络科技有限公司，并保留所有权利。
- * 网站地址: http://www.ecshop.com；
- * ----------------------------------------------------------------------------
- * 这不是一个自由软件！您只能在不用于商业目的的前提下对程序代码进行修改和
- * 使用；不允许对程序代码以任何形式任何目的的再发布。
- * ============================================================================
- * $Author: douqinghua $
- * $Id: upop.php 17063 2010-03-25 06:35:46Z douqinghua $
+ * ECSHOP 银联在线支付主文件
+	插件名称：银联在线支付(UPOP)插件 FOR ECSHOP
+	插件版本：1.0
+	插件作者：潍坊中脉信息技术部
+	插件编码：UTF-8
+	适用版本：ECSHOP 2.7.3
+	开发时间：2015.11.6
+	支持站点：www.wfzmxx.com
+	反馈邮箱/QQ账号：3138711793@qq.com
  */
-
+error_reporting(E_ALL);
 if (!defined('IN_ECS'))
 {
     die('Hacking attempt');
@@ -46,26 +45,27 @@ if (isset($set_modules) && $set_modules == TRUE)
     $modules[$i]['is_online']  = '1';
 
     /* 作者 */
-    $modules[$i]['author']  = 'ECSHOP TEAM';
+    $modules[$i]['author']  = '潍坊中脉信息技术部';
 
     /* 网址 */
-    $modules[$i]['website'] = 'http://www.ecshop.com';
+    $modules[$i]['website'] = 'http://www.wfzmxx.com';
 
     /* 版本号 */
     $modules[$i]['version'] = '1.0.0';
 
     /* 配置信息 */
     $modules[$i]['config'] = array(
-        array('name' => 'upop_merAbbr', 'type' => 'text', 'value' => '商户名称'),
-        array('name' => 'upop_account', 'type' => 'text', 'value' => ''),
-        array('name' => 'upop_security_key', 'type' => 'text', 'value' => ''),
+		array('name' => 'upop_merid', 'type' => 'text', 'value' => '777290058110097'),
+		array('name' => 'upop_sin_cert_pwd', 'type' => 'text', 'value' => '000000'),
+		array('name' => 'sdk_front_trans_url','type'=>'text','value'=>'https://101.231.204.80:5000/gateway/api/frontTransReq.do'),
+		array('name' => 'sdk_back_trans_url','type'=>'text','value'=>'https://101.231.204.80:5000/gateway/api/backTransReq.do'),
     );
 
     return;
 }
 
 /**
- * 类
+ * 中国银联支付接口类
  */
 class UPOP
 {
@@ -77,60 +77,42 @@ class UPOP
 
     function get_code($order, $payment)
     {
-        // 初始化变量
-        if (!defined('EC_CHARSET'))
-        {
-            $charset = 'UTF-8';
-        }
-        else
-        {
-            $charset = strtoupper(EC_CHARSET);
-        }
 
-        $front_pay_url         = 'https://unionpaysecure.com/api/Pay.action';
-        $security_key          = $payment['upop_security_key'];
-        $merId                 = $payment['upop_account'];
-        $orderNumber           = $order['order_sn'] . '-' . $this->_formatSN($order['log_id']);	
-        $frontEndUrl           = return_url(basename(__FILE__, '.php'));
-        $backEndUrl            = return_url(basename(__FILE__, '.php'));
-        $merAbbr               = $payment['upop_merAbbr'];
+		// 初始化变量
+		$lib_path		= ROOT_PATH . 'includes/modules/payment/unionpay/';
+		include_once($lib_path . 'common.php');
 
-        $params = array(
-                "version"            =>  '1.0.0',                      //接口版本
-                "signMethod"         =>  'md5',                        //加密方式
-                "charset"            =>  $charset,                     //编码
-                "transType"          =>  '01',                         //交易类型
-                "origQid"            =>  '',
-                "merId"              =>  $merId,                       //收款账号
-                "merAbbr"            =>  $merAbbr,                     //商户名称
-                "acqCode"            =>  '',
-                "merCode"            =>  '',
-                "commodityUrl"       =>  '',                           //商品url
-                "commodityName"      =>  '',                           //商品名字
-                "commodityUnitPrice" =>  '',                           //商品单价
-                "commodityQuantity"  =>  '',                           //商品数量
-                "commodityDiscount"  =>  '',
-                "transferFee"        =>  '',
-                "orderNumber"        =>  $orderNumber,                 //订单号，必须唯一
-                "orderAmount"        =>  $order['order_amount'] * 100, //交易金额 转化为分
-                "orderCurrency"      =>  '156',                        //交易币种，CURRENCY_CNY=>人民币
-                "orderTime"          =>  date('YmdHis'),               //交易时间, YYYYmmhhddHHMMSS
-                "customerIp"         =>  $_SERVER['REMOTE_ADDR'],      //用户IP
-                "customerName"       =>  '',
-                "defaultPayType"     =>  '',
-                "defaultBankNumber"  =>  '',
-                "transTimeout"       =>  '',
-                "frontEndUrl"        =>  $frontEndUrl,                 // 前台回调URL
-                "backEndUrl"         =>  $backEndUrl,                  // 后台回调URL
-                "merReserved"        =>  ''             
-        );
-        
-        $params['signature']    =$this->sign($params, $security_key,'md5');
-        
-        $button = "<input type='submit' value='" . $GLOBALS['_LANG']['upop_button'] . "' />";
-        $html = $this->create_html($params,$front_pay_url,$button);
-
-        return $html;
+		$config=array(
+		   'sdk_sign_cert_pwd'=>$payment['upop_sin_cert_pwd'],
+		   'sdk_front_trans_url'=>$payment['sdk_front_trans_url'],
+		   'sdk_back_trans_url'=>$payment['sdk_back_trans_url'],
+		);
+		
+		$unionpay=new unionpay($config);
+		
+		$params = array(
+				//以下信息非特殊情况不需要改动
+				'version' => '5.0.0',                 //版本号
+				'encoding' => 'utf-8',				  //编码方式
+				'certId' => $unionpay->getSignCertId (),	      //证书ID
+				'txnType' => '01',				      //交易类型
+				'txnSubType' => '01',				  //交易子类
+				'bizType' => '000201',				  //业务类型
+				'frontUrl' =>  return_url(basename(__FILE__, '.php')),  //前台通知地址
+				'backUrl'  =>   return_url(basename(__FILE__, '.php')),	//后台通知地址
+				'signMethod' => '01',	              //签名方法
+				'channelType' => '08',	              //渠道类型，07-PC，08-手机
+				'accessType' => '0',		          //接入类型
+				'currencyCode' => '156',	          //交易币种，境内商户固定156
+				
+				//TODO 以下信息需要填写
+				'merId' => $payment['upop_merid'],		//商户代码，请改自己的测试商户号，此处默认取demo演示页面传递的参数
+				'orderId' => $order['order_sn'],	//商户订单号，8-32位数字字母，不能含“-”或“_”，此处默认取demo演示页面传递的参数，可以自行定制规则
+				'txnTime' => date("YmdHis",time()),	//订单发送时间，格式为YYYYMMDDhhmmss，取北京时间，此处默认取demo演示页面传递的参数
+				'txnAmt' => $order['order_amount'] * 100,	//交易金额，单位分，此处默认取demo演示页面传递的参数
+			);
+			$unionpay->sign ( $params );
+			return $unionpay->create_html ( $params);
     }
 
     /**
@@ -138,131 +120,52 @@ class UPOP
      */
     function respond()
     {
-            $payment        = get_payment('upop');
+		// 初始化变量
+		$lib_path		= ROOT_PATH . 'includes/modules/payment/unionpay/';
+		// 包含库接口文件
+		include_once($lib_path . 'common.php');
+		
+        $payment        = get_payment('upop');
+		$config=array(
+		   'sdk_sign_cert_pwd'=>$payment['upop_sin_cert_pwd'],
+		   'sdk_front_trans_url'=>$payment['sdk_front_trans_url'],
+		   'sdk_back_trans_url'=>$payment['sdk_back_trans_url'],
+		);
+		$unionpay=new unionpay($config);		
+	
+		$order_sn=$_POST['orderId'];
+		$payment_amount=$_POST['settleAmt'];
+		
+		if($_POST ['respCode']=='00'||$_POST ['respCode']=='A6'){
+			
+			//检查签名是否正确
+			if(!$unionpay->verify($_POST)){
+				return false;
+			}
+			
+			// 检查价格是否一致。
+			$sql = "SELECT p.order_amount FROM " . $GLOBALS['ecs']->table('pay_log') . " AS p LEFT JOIN " . $GLOBALS['ecs']->table('order_info') . " AS o ON p.order_id = o.order_id WHERE o.order_sn = '"
+			. $order_sn . "'";
+			$order_amount = $GLOBALS['db']->getOne($sql) * 100;
 
-            $arr_args = array();
-            $arr_reserved = array();
-
-            if (is_array($_POST)) 
-            {
-                $arr_args       = $_POST;
-                $cupReserved    = isset($arr_args['cupReserved']) ? $arr_args['cupReserved'] : '';
-                parse_str(substr($cupReserved, 1, -1), $arr_reserved); //去掉前后的{}
-            }
-            else 
-            {
-                $cupReserved = '';
-                $pattern = '/cupReserved=(\{.*?\})/';
-                if (preg_match($pattern, $_POST, $match)) { //先提取cupReserved
-                   $cupReserved = $match[1];
-                }
-                //将cupReserved的value清除(因为含有&, parse_str没法正常处理)
-                $args_r         = preg_replace($pattern, 'cupReserved=', $_POST);
-                parse_str($args_r, $arr_args);
-                $arr_args['cupReserved'] = $cupReserved;
-                parse_str(substr($cupReserved, 1, -1), $arr_reserved); //去掉前后的{}
-            }
-            //提取服务器端的签名
-            if (!isset($arr_args['signature']))
-            {
-                 return false;
-            }
-     
-            //验证签名
-            $signature=$this->sign($arr_args, $payment['upop_security_key'],'md5');
-            if ($signature != $arr_args['signature']) 
-            {
-                return false;
-            }
-
-            $arr_ret = array_merge($arr_args, $arr_reserved);
-            unset($arr_ret['cupReserved']);
-
-            if ($arr_ret['respCode'] != '00') 
-            {
-                return false;
-            }
-            if(!strpos($arr_ret['orderNumber'], '-')) 
-            {
-                return false;
-            }
-            $order_sn_arr = explode('-', $arr_ret['orderNumber']);
-            
-            $order_sn    = $order_sn_arr['0'];
-            $pay_id = intval($order_sn_arr['1']);
-            $payment_amount = intval($arr_ret['settleAmount']);
-          
-            // 检查商户账号是否一致。
-            if ($payment['upop_account'] != $arr_ret['merId'])
-            {
-               return false;
-            }
-            // 检查价格是否一致
-            if (!check_money($pay_id, $payment_amount/100))
-            {
-               
-               return false;
-            }
-            // 如果未支付成功。
-            if ($arr_ret['respCode'] != '00')
-            {
-               return false;
-            }
-
-            $action_note = $arr_ret['respCode'] . ':' 
-                    . $arr_ret['respMsg'] 
-                    . $GLOBALS['_LANG']['upop_txn_id'] . ':' 
-                    . $arr_ret['qid'];
-
-            // 完成订单。
-            order_paid($pay_id, PS_PAYED, $action_note);
-
-            //告诉用户交易完成
-            return true;
-
+			if ($order_amount != $payment_amount)
+			{
+				return false;
+			}			
+			
+			$sql="select log_id from ".$GLOBALS['ecs']->table('order_info')." as o right join ".$GLOBALS['ecs']->table('pay_log')." as p".
+			" on o.order_id=p.order_id where o.order_sn='{$order_sn}'";
+			$log_id=$GLOBALS['db']->getOne($sql);
+			
+			if($log_id){
+				order_paid($log_id,PS_PAYED);
+				return true;
+			}else{
+				return false;
+			}	
+		}
+		
+		return false;
     }
-    /**
-    * 格式订单号
-    */
-    function _formatSN($sn)
-    {
-        return str_repeat('0', 9 - strlen($sn)) . $sn;
-    }
-    function create_html($params,$front_pay_url,$button)
-    {
-        $html = <<<eot
-    <br />
-    <form style="text-align:center;" id="pay_form" name="pay_form" action="{$front_pay_url}" method="post" target="_blank">
-eot;
-        foreach ($params as $key => $value) 
-        {
-            $html .= " <input type=\"hidden\" name=\"{$key}\" id=\"{$key}\" value=\"{$value}\" />\n";
-        }
-        $html .= $button . "</form><br />";
-        return $html;
-    }
-    function sign($params,$security_key,$sign_method)
-    {
-        if (strtolower($sign_method) == "md5") 
-        {
-            ksort($params);
-            $sign_str = "";
-            $sign_ignore_params=array('bank','signMethod','signature');
-            foreach ($params as $key => $val)
-            {
-                if (in_array($key,$sign_ignore_params)) 
-                {
-                    continue;
-                }
-                $sign_str .= sprintf("%s=%s&", $key, $val);
-            }
-            return md5($sign_str . md5($security_key));
-        }
-        else 
-        {
-            exit("Unknown sign_method set in quickpay_conf");
-        }
-    }
-
 }
 ?>
